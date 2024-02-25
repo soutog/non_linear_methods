18a026bcfc0d843ead13bcdc7467f68748098f7c
Gabriel Souto <soutog4@gmail.com>
2024-02-25 18:35:00 -0300

    conjugated gradient & modified newton

--- conjugated_gradient.py
+++ conjugated_gradient.py
@@ -1,13 +1,13 @@
 import time
 
 from functions import himmelblau, rosenbrock
-from methods import modified_newton
+from methods import conjugated_gradient
 from plots import plot_method
 
 if __name__ == "__main__":
 
     print("\n======================================")
-    print("MODIFIED NEWTON - Himmelblau")
+    print("CONJUGATED GRADIENT - Himmelblau")
 
     begin_time = time.time()
     # generate gradient vector
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     x0 = [-6.0, -6.0]
 
     # Run gradient descent optimization
-    x, iteration, trajectory = modified_newton(himmelblau, x0)
+    x, iteration, trajectory = conjugated_gradient(himmelblau, x0)
     min_value = himmelblau(x)
 
     end_time = time.time()
@@ -27,18 +27,18 @@ if __name__ == "__main__":
     print(f"Total Time: {end_time-begin_time}")
     print(f"Average Iteration Time: {(end_time-begin_time)/iteration}")
 
-    plot_method(trajectory, "modified_newton_himmelblau", x, "himmel")
+    plot_method(trajectory, "conjugated_gradient_himmelblau", x, "himmel")
 
     print("\n======================================")
-    print("MODIFIED NEWTON - Rosenbrock")
+    print("CONJUGATED GRADIENT - Rosenbrock")
 
     begin_time = time.time()
     # generate gradient vector
     # Initial guess
-    x0 = [0.0, 0.0]
+    x0 = [2.0, 0.0]
 
     # Run gradient descent optimization
-    x, iteration, trajectory = modified_newton(rosenbrock, x0)
+    x, iteration, trajectory = conjugated_gradient(rosenbrock, x0)
     min_value = rosenbrock(x)
 
     end_time = time.time()
@@ -50,4 +50,4 @@ if __name__ == "__main__":
     print(f"Total Time: {end_time-begin_time}")
     print(f"Average Iteration Time: {(end_time-begin_time)/iteration}")
 
-    plot_method(trajectory, "modified_newton_rosenbrock", x, "rosen")
+    plot_method(trajectory, "conjugated_gradient_rosenbrock", x, "rosen")
--- methods.py
+++ methods.py
@@ -1,8 +1,7 @@
 import numdifftools as nd
 import numpy as np
-from scipy.optimize import line_search
-
 from line_searchs import armijo_search, line_search
+import copy
 
 
 def gradient_descent(f, x, sigma=0.02, epsilon=1e-6, max_iter=100000):
@@ -110,44 +109,20 @@ def modified_newton(f, x, sigma=0.02, epsilon=1e-6, max_iter=100000):
 
     df = nd.Gradient(f)
     f_hessian = nd.Hessian(f)
-    grad_fk = df(x)
-    grad_fk_norm = np.linalg.norm(df(x))
-    f_hessian_xk = f_hessian(x)
 
     trajectory = [x]
     iter = 0
 
     for _ in range(max_iter):
-        gradient = -1 * df(x)
+        # gradient = -1 * df(x)
 
-        if (
-            (np.linalg.norm(f(x)) > 1e16)
-            or (grad_fk_norm > 1e16)
-            or (np.linalg.norm(f_hessian_xk) > 1e16)
-        ):
-            print("\nErro: Overflow\n")
-            break
+        identity_matrix = np.eye(len(x)) * 0.001
+        f_hessian_modified = f_hessian(x) + identity_matrix
 
-        # Não invertível
-        if abs(np.linalg.det(f_hessian_xk)) <= 1e-3:
-            break
+        pk = -np.linalg.solve(f_hessian_modified, df(x))
 
-        pk = -np.linalg.solve(f_hessian(x), df(x))
-        learning_rate = 1
-
-        # Se estiver longe do ponto, a matriz Hessiana pode ser não definida positiva.
-        # Se isso acontecer, seguir pelo Método do Gradiente:
-        if not (
-            np.dot(pk, grad_fk) < (-0.001 * np.linalg.norm(df(x)) * np.linalg.norm(pk))
-        ):
-            # gradient
-            gradient = -1 * df(x)
-
-            # Linear Search with Armijo Condition
-            learning_rate = armijo_search(f, x, gradient, sigma)
-        else:
-            # Linear Search with Armijo Condition
-            learning_rate = armijo_search(f, x, pk, sigma)
+        learning_rate = armijo_search(f, x, pk, sigma)
+        # learning_rate = line_search(f, x)
 
         x_new = x + learning_rate * pk
         if (abs(x_new - x) < epsilon).all():
@@ -157,13 +132,53 @@ def modified_newton(f, x, sigma=0.02, epsilon=1e-6, max_iter=100000):
         x = x_new
         trajectory.append(x)
 
-    if abs(np.linalg.det(f_hessian_xk)) <= 1e-3:
-        print("\nMatriz Hessiana não invertível. Mudar o ponto inicial.\n")
-        return [], [], []
+    return x, iter, np.array(trajectory)
 
-    elif np.min(np.linalg.eigvals(f_hessian_xk)) <= 1e-3:
-        print(
-            "\nMatriz Hessiana não PD. Mudar o ponto inicial. Ponto de sela encontrado.\n"
-        )
+
+def conjugated_gradient(f, x, sigma=0.02, epsilon=1e-6, max_iter=100000):
+    iter = 0
+    trajectory = [x]
+    df = nd.Gradient(f)
+    x0 = copy.deepcopy(x)
+
+    for i in range(max_iter):
+
+        if i == 0:
+            gradient = -1 * df(x0)
+
+            learning_rate = armijo_search(f, x0, gradient, sigma)
+            # learning_rate = line_search(f, x)
+
+            x_new = x0 + learning_rate * gradient
+
+            if (abs(x_new - x0) < epsilon).all():
+                break
+
+            iter += 1
+            x = x_new
+            trajectory.append(x)
+        else:
+            x1 = copy.deepcopy(x0)  # anterior
+            xk = copy.deepcopy(x)  # corrente
+
+            gradient = -1 * df(xk)
+
+            pk = gradient + (
+                np.dot(np.transpose(df(xk) - df(x1)), df(xk))
+                / np.dot(np.transpose(df(x1)), df(x1))
+            ) * df(x1)
+
+            learning_rate = armijo_search(f, x, pk, sigma)
+            # learning_rate = line_search(f, x)
+
+            x_new = x + learning_rate * pk
+
+            if (abs(x_new - x) < epsilon).all():
+                break
+
+            iter += 1
+            x1 = copy.deepcopy(xk)
+            x = copy.deepcopy(x_new)
+            trajectory.append(x)
 
     return x, iter, np.array(trajectory)
